54a0d0f06f3c6da32f62308b6c2b5b2b2c941c53
Dean <deanhildreth2@gmail.com>
2023-11-13 12:01:59 -0500

    Title and name update plus Jimmy swift logo

---
+++ SwiftUIStoryTemplate/MainPageViewChapters.swift
@@ -0,0 +1,18 @@
+//
+//  MainPageViewChapters.swift
+//  SwiftUIStoryTemplate
+//
+//  Created by Dean Hildreth II on 11/13/23.
+//
+
+import SwiftUI
+
+struct MainPageViewChapters: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainPageViewChapters()
+}
