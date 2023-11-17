4d415a85f8249d2b43392a60acb4681b0631d664
Anna <Zvereva.krsk@gmail.com>
2023-11-17 08:22:11 -0500

    change content view

--- SwiftUIStoryTemplate/ContentView.swift
+++ SwiftUIStoryTemplate/ContentView.swift
@@ -4,49 +4,71 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        ZStack{
-            Image("Background")
-                .resizable()
-                .scaledToFill()
-                .ignoresSafeArea()
-                .opacity(0.7)
-            
-            
-            VStack{
-                
-                Text ("The Evolution of Jimmy Swift")
-                    .bold()
-                    .font(.title2)
-                    .padding(.bottom)
-                    Spacer()
-                
-                Text ("Authors")
-                    .bold()
-                Text ("Anna, Dean, Chantelle, Shaundra")
-                    .bold()
-                    .font(.largeTitle)
-                    .frame(width: 400)
-                HStack {
-                    Image ("Jimmy Swift")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 50, height: 50)
-                        .padding()
-                    Image ("Sara Ruby")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 50, height: 50)
-                        .padding()
-                    Image ("Tommy Python")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 50, height: 50)
-                        .padding()
+        TabView {
+            Chapter1View()
+                .tabItem {
+                    Label("Chapter 1", systemImage: "1.circle")
                 }
-                
-            }
-            }
-            
+            Chapter2View()
+                .tabItem {
+                    Label("Chapter 2", systemImage: "2.circle")
+                }
+            Chapter3View()
+                .tabItem {
+                    Label("Chapter 3", systemImage: "3.circle")
+                }
+            Chapter4View()
+                .tabItem {
+                    Label("Chapter 4", systemImage: "4.circle")
+                }
+            Chapter5View()
+                .tabItem {
+                    Label("Chapter 5", systemImage: "5.circle")
+                }
+        }
+//        ZStack{
+//            Image("Background")
+//                .resizable()
+//                .scaledToFill()
+//                .ignoresSafeArea()
+//                .opacity(0.7)
+//            
+//            
+//            VStack{
+//                
+//                Text ("The Evolution of Jimmy Swift")
+//                    .bold()
+//                    .font(.title2)
+//                    .padding(.bottom)
+//                    Spacer()
+//                
+//                Text ("Authors")
+//                    .bold()
+//                Text ("Anna, Dean, Chantelle, Shaundra")
+//                    .bold()
+//                    .font(.largeTitle)
+//                    .frame(width: 400)
+//                HStack {
+//                    Image ("Jimmy Swift")
+//                        .resizable()
+//                        .scaledToFit()
+//                        .frame(width: 50, height: 50)
+//                        .padding()
+//                    Image ("Sara Ruby")
+//                        .resizable()
+//                        .scaledToFit()
+//                        .frame(width: 50, height: 50)
+//                        .padding()
+//                    Image ("Tommy Python")
+//                        .resizable()
+//                        .scaledToFit()
+//                        .frame(width: 50, height: 50)
+//                        .padding()
+//                }
+//                
+//            }
+//            }
+//            
         }
     }
 
